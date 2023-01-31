2a496739a6d577ac14c251aa839d674a7ca84bab
Alana Barrett-Frew <alanabarrett-frew@alanas-laptop.home>
2023-01-31 13:50:50 +0000

    css styling added and debugged so that data does not repeat (append--> text)

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# Weather-Dashboard - Server-Side APIs:
+# Weather-Dashboard - Server-Side APIs
+
 A weather dashboard that runs in the browser and features dynamically updated HTML and CSS
 
 ## Your Task
@@ -7,9 +8,9 @@ Server APIs allow developers to access their data and functionality by making re
 
 Use the [5 Day Weather Forecast](https://openweathermap.org/forecast5) to retrieve weather data for cities. The link should take you to a guide on how to use the 5 Day Forecast API. You will need to register for an API key in order to use this API. After registering for a new API key, you may need to wait up to 2 hours for that API key to activate.
 
-You will use `localStorage` to store any persistent data. 
+You will use `localStorage` to store any persistent data.
 
-https://alanabf.github.io/Weather-Dashboard/
+<https://alanabf.github.io/Weather-Dashboard/>
 
 ## User Story
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+
     // empty array for the cities to go in
     cities = [];
 
@@ -57,91 +58,91 @@ $(document).ready(function () {
             var iconURL = "http://openweathermap.org/img/w/" + iconcode + ".png";
             $(".weather-icon").attr("src", iconURL);
             // adds todays date which updates on refresh
-            var dateToday = moment().format("dddd, MMMM Do YYYY");
-            $("#date-today").append(dateToday);
+            var dateToday = moment().format("(dddd, MMMM Do YYYY)");
+            $("#date-today").text(dateToday);
             //retrieving the temp, wind and humidity data from the api
             var tempC = Math.floor(response.list[0].main.temp - 273.15);
             var wind = response.list[0].wind.speed;
             var humidity = response.list[0].main.humidity;
             //append the data to the web application
-            $("#image").append(iconURL);
-            $("#city-name").append(response.city.name)
+            $("#image").text(iconURL);
+            $("#city-name").text(response.city.name)
             //append the data to web application
-            $("#temperature").append("Temp: " + tempC + "°C")
-            $("#wind").append("Wind Speed: " + wind + "MPH")
-            $("#humidity").append("Humidity: " + humidity + "%")
+            $("#temperature").text("Temp: " + tempC + "°C")
+            $("#wind").text("Wind Speed: " + wind + "MPH")
+            $("#humidity").text("Humidity: " + humidity + "%")
             //call data for 5 day forecast
             //need [8][16][24][32][39]
             // DAY 1
             var day1 = moment().add(1, 'days').format("DD/MM/YYYY");
-            $("#day1").append(day1);
+            $("#day1").text(day1);
             iconcode = response.list[8].weather[0].icon;
             iconURL = "http://openweathermap.org/img/w/" + iconcode + ".png";
             $(".weather1").attr("src", iconURL);
             var tempC = Math.floor(response.list[8].main.temp - 273.15);
             var wind = response.list[8].wind.speed;
             var humidity = response.list[8].main.humidity;
-            $("#image").append(iconURL);
+            $("#image").text(iconURL);
             //append data to webpage
-            $("#temp-day1").append("Temp: " + tempC + "°C")
-            $("#wind-day1").append("Wind Speed: " + wind + "MPH")
-            $("#humidity-day1").append("Humidity: " + humidity + "%")
+            $("#temp-day1").text("Temp: " + tempC + "°C")
+            $("#wind-day1").text("Wind Speed: " + wind + "MPH")
+            $("#humidity-day1").text("Humidity: " + humidity + "%")
             // DAY 2
             var day2 = moment().add(2, 'days').format("DD/MM/YYYY");
-            $("#day2").append(day2);
+            $("#day2").text(day2);
             iconcode = response.list[16].weather[0].icon;
             iconURL = "http://openweathermap.org/img/w/" + iconcode + ".png";
             $(".weather2").attr("src", iconURL);
             var tempC = Math.floor(response.list[16].main.temp - 273.15);
             var wind = response.list[16].wind.speed;
             var humidity = response.list[16].main.humidity;
-            $("#image").append(iconURL);
+            $("#image").text(iconURL);
             //append data to webpage
-            $("#temp-day2").append("Temp: " + tempC + "°C")
-            $("#wind-day2").append("Wind Speed: " + wind + "MPH")
-            $("#humidity-day2").append("Humidity: " + humidity + "%")
+            $("#temp-day2").text("Temp: " + tempC + "°C")
+            $("#wind-day2").text("Wind Speed: " + wind + "MPH")
+            $("#humidity-day2").text("Humidity: " + humidity + "%")
             // DAY 3
             var day3 = moment().add(3, 'days').format("DD/MM/YYYY");
-            $("#day3").append(day3);
+            $("#day3").text(day3);
             iconcode = response.list[24].weather[0].icon;
             iconURL = "http://openweathermap.org/img/w/" + iconcode + ".png";
             $(".weather3").attr("src", iconURL);
             var tempC = Math.floor(response.list[24].main.temp - 273.15);
             var wind = response.list[24].wind.speed;
             var humidity = response.list[24].main.humidity;
-            $("#image").append(iconURL);
+            $("#image").text(iconURL);
             //append data to webpage
-            $("#temp-day3").append("Temp: " + tempC + "°C")
-            $("#wind-day3").append("Wind Speed: " + wind + "MPH")
-            $("#humidity-day3").append("Humidity: " + humidity + "%")
+            $("#temp-day3").text("Temp: " + tempC + "°C")
+            $("#wind-day3").text("Wind Speed: " + wind + "MPH")
+            $("#humidity-day3").text("Humidity: " + humidity + "%")
             // DAY 4
             var day4 = moment().add(4, 'days').format("DD/MM/YYYY");
-            $("#day4").append(day4);
+            $("#day4").text(day4);
             iconcode = response.list[32].weather[0].icon;
             iconURL = "http://openweathermap.org/img/w/" + iconcode + ".png";
             $(".weather4").attr("src", iconURL);
             var tempC = Math.floor(response.list[32].main.temp - 273.15);
             var wind = response.list[32].wind.speed;
             var humidity = response.list[32].main.humidity;
-            $("#image").append(iconURL);
+            $("#image").text(iconURL);
             //append data to webpage
-            $("#temp-day4").append("Temp: " + tempC + "°C")
-            $("#wind-day4").append("Wind Speed: " + wind + "MPH")
-            $("#humidity-day4").append("Humidity: " + humidity + "%")
+            $("#temp-day4").text("Temp: " + tempC + "°C")
+            $("#wind-day4").text("Wind Speed: " + wind + "MPH")
+            $("#humidity-day4").text("Humidity: " + humidity + "%")
             // DAY 5
             var day5 = moment().add(5, 'days').format("DD/MM/YYYY");
-            $("#day5").append(day5);
+            $("#day5").text(day5);
             iconcode = response.list[39].weather[0].icon;
             iconURL = "http://openweathermap.org/img/w/" + iconcode + ".png";
             $(".weather5").attr("src", iconURL);
             var tempC = Math.floor(response.list[39].main.temp - 273.15);
             var wind = response.list[39].wind.speed;
             var humidity = response.list[39].main.humidity;
-            $("#image").append(iconURL);
+            $("#image").text(iconURL);
             //append data to webpage
-            $("#temp-day5").append("Temp: " + tempC + "°C")
-            $("#wind-day5").append("Wind Speed: " + wind + "MPH")
-            $("#humidity-day5").append("Humidity: " + humidity + "%")
+            $("#temp-day5").text("Temp: " + tempC + "°C")
+            $("#wind-day5").text("Wind Speed: " + wind + "MPH")
+            $("#humidity-day5").text("Humidity: " + humidity + "%")
         });
     }
 
